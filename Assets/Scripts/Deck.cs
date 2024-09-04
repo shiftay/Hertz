611e9d550acf667fe211b1b9d9309af27f74d133
Stephen Roebuck <shroebuck@gmail.com>
2024-09-04 14:07:22 -0700

    Odin

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Deck : MonoBehaviour
+public class Deck
 {
     public List<Card> cards = new List<Card>();
 }
