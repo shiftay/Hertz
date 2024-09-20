7baa0698006cc1638a943f99390e2a331161c222
Stephen Roebuck <shroebuck@gmail.com>
2024-09-20 15:48:03 -0700

    Settings / Shop > IMPLEMENT comment setup > Basic idea of what will need to be addressed.

---
+++ Assets/Scripts/GamePlay/RoguelikeElements/Shop.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    /*
+        IMPLEMENT  
+        View Deck   
+                    > Clickable and opens up the deck, and shows currently the cards inside it.
+                    > Shows a count of the different enhancements on the side
+        
+        Buy Individual Card
+                    > Purchase a card and replaces it into the deck
+                    IF  Replacing another enhanced card show the player so they can decide to continue and sell the card it's replacing
+                    OR  Cancel the purchase
+        
+        Buy Trinket
+                    > Trinkets until trinket slots are filled.
+        
+        Buy Featured Pack
+                    > Pack slides to center
+                    > Rips open
+                    > Player can choose what to do with however many cards are inside the pack
+                    > Sell the cards
+                    > Replace cards in the deck
+
+        IDEA BASE CARDS DO NOT SELL FOR ANYTHING
+
+        Reroll
+                    > Reroll starts at X
+                    > Goes up each reroll.
+                    > Reroll Trinkets and individual cards.
+                        > Cards will flip over & Maybe Morph from Marvel snap scrunch up then turn back over.
+                        > Trinkets will poof and then poof back.        
+
+        
+        Continue
+                    > Button to take you to the next round.
+    */
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    /*
+        IMPLEMENT
+
+        Volume      
+                    > Music
+                    > SFX
+        
+        Gameplay speed
+                    > Allow speeding up of animations or CPU Play
+                    > Enum { SLOW, NORMAL, FAST }
+
+        IDEA Bindings    
+                    > Allow remapping of controller buttons.
+
+
+        Reset 
+                    > Reset "Progress"
+
+        Credits
+                    > Loads a page for "Credits"
+    */
+}
